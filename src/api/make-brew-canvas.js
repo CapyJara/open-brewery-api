03c29ab41093bd265d9d530910ef3155bbea3d92
UncleJerry <jared.myhrberg@gmail.com>
2019-03-13 15:43:12 -0700

    added heart to template

--- src/api/make-brew-canvas.js
+++ src/api/make-brew-canvas.js
@@ -1,14 +1,17 @@
+import { auth, favoritesByUserRef } from '../firebase/firebase.js';
+
 export function makeBreweryCanvas(brewery) {
     const html = `
     <div id="brewery">
         <a href="${brewery.website_url}">
-            <h2>${brewery.name}</h2>
+            <h2>${brewery.name}</h2>    
         </a>
         <h4>${brewery.brewery_type}</h4>
         <div>
             <h3>${brewery.city}</h3>
             <h3>${brewery.state}</h3>
         </div>
+        <img src="../../assets/fav-unselected.svg" id="favorite-icon">
     </div>
     `;
 
@@ -26,6 +29,11 @@ export default function loadCharacters(body) {
 
     body.forEach(brewery => {
         const dom = makeBreweryCanvas(brewery);
+
+
+
+
+
         breweryContainer.appendChild(dom);
     });
 
